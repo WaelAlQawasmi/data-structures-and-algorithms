b6f026cec844d077b47c748bfdfcdf47ff59b98b
waelalq <wael.abuomr@gmail.com>
2022-03-06 22:19:29 +0200

    - [x] Top-level README “Table of Contents” is updated  - [x] README for this challenge is complete        - [X] Summary, Description, Approach & Efficiency, Solution        - [X] Picture of whiteboard        - [x] Link to code  - [x] Feature tasks for this challenge are completed  - [x] Unit tests written and passing        - [x] “Happy Path” - Expected outcome        - [x] Expected failure        - [x] Edge Case (if applicable/obvious)

---
+++ java/linked-list-insertions/lib/src/main/java/linked/list/insertions/Node.java
@@ -0,0 +1,10 @@
+package linked.list.insertions;
+
+public class Node<T> {
+T value; // to decelerate  variable can have any data type
+Node next; //to declerate variable can have an abject of class
+
+    public Node(T value) {
+        this.value =value ;
+    }
+}
