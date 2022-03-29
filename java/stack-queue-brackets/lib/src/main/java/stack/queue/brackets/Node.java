55cca0aa554a09be6a7a24ea73dc210d515d7a5b
Wa'el K Al Qawasmi <WAEL.ABUOMR@GMAIL.COM>
2022-03-29 10:23:53 +0200

    Update Node.java

--- java/stack-queue-brackets/lib/src/main/java/stack/queue/brackets/Node.java
+++ java/stack-queue-brackets/lib/src/main/java/stack/queue/brackets/Node.java
@@ -1,2 +1,38 @@
-package stack.queue.brackets;public class Node {
+package stack.queue.brackets;
+
+
+public class  Node {
+    private final char value;
+
+    private Node  next;
+
+
+
+    public Node(char value) {
+        this.value = value;
+
+    }
+
+
+    public void setNext(Node  next) {
+        this.next = next;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+
+
+    public Node  getNext() {
+        return next;
+    }
+
+    @Override
+    public String toString() {
+        return "stackNode{" +
+                "value='" + value + '\'' +
+                '\'' +
+                '}';
+    }
 }
