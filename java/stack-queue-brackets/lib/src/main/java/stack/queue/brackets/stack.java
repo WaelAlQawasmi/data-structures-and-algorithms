9fa3b504d11939d6713035119064e2eb519f269d
Wa'el K Al Qawasmi <WAEL.ABUOMR@GMAIL.COM>
2022-03-29 10:24:32 +0200

    Update stack.java

--- java/stack-queue-brackets/lib/src/main/java/stack/queue/brackets/stack.java
+++ java/stack-queue-brackets/lib/src/main/java/stack/queue/brackets/stack.java
@@ -1,2 +1,84 @@
-package stack.queue.brackets;public class Stack {
-}
+package stack.queue.brackets;
+
+
+
+    public class stack {
+        private  Node  top;
+
+
+
+        public stack() {
+            this.top = null;
+        }
+
+
+        public boolean isEmpty(){
+            if(this.top==null){
+                return true;
+            }
+            else {
+                return  false;
+            }
+        }
+
+
+
+        public Node   push(Node  newStackNode){
+            if (isEmpty()){
+                this.top=newStackNode;
+                return newStackNode;
+            }
+            else {
+                newStackNode.setNext(top);
+                top=newStackNode;
+                return  newStackNode;
+
+            }
+        }
+
+
+        public char pop(){
+            if (!isEmpty()){
+
+                this.top=this.top.getNext();
+                if(this.top!= null){
+                    return  this.top.getValue();}
+                else {
+                    return 0;
+                }
+            }
+            else {
+                System.out.println("it is empty");
+                return 0;
+            }
+
+        }
+
+
+
+
+        public char peek()
+        {
+            if (!isEmpty()){
+                return this.top.getValue();
+            }
+            else
+                return 0;
+        }
+
+        @Override
+        public String toString() {
+            Node pointer= this.top;
+            String OUTPUT="";
+            while (pointer!=null){
+                OUTPUT+=pointer.getValue()+" =>";
+                pointer=pointer.getNext();
+            }
+            return "stack{" +
+                    OUTPUT +"null"+
+                    '}';
+        }
+    }
+
+
+
