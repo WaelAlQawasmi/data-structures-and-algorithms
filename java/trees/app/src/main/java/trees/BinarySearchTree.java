3319565a1a761d3120c063adf588551d7a94b24d
waelalq <wael.abuomr@gmail.com>
2022-03-30 22:50:52 +0200

    ### Trees - [x] Create a Node class - [x] Create a Binary Tree class - [x] Define a method for each of the depth first traversals: pre order in order post order which returns an array of the values, ordered appropriately. - [x] Create a Binary Search Tree class with the following additional methods: Add and Contains
    - [x] Can successfully instantiate an empty tree
    - [x]
    Can successfully instantiate a tree with a single root node
    - [x] For a Binary Search Tree, can successfully add a left child and right child properly to a node
    - [x] Can successfully return a collection from a preorder traversal
    - [x] Can successfully return a collection from an inorder traversal
    - [x] Can successfully return a collection from a postorder traversal
    - [x] false for the contains method, given an existing or non-existing node value

---
+++ java/trees/app/src/main/java/trees/BinarySearchTree.java
@@ -0,0 +1,138 @@
+package trees;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    private  Node<T> root;
+    private int size;
+
+    public String insert(T data) {
+        if (isTreeEmpty()) {
+            this.root = new BinaryNode <> (data);
+
+            size++;
+            return "root";
+        } else {
+           return insertNew(data, root);
+        }
+    }
+
+    private String insertNew(T data, Node<T> root) {
+
+       if (data.compareTo (root.getData()) < 0)
+      {
+          if (root.getLeftNode () != null)
+      {
+          insertNew(data, root.getLeftNode());
+      }
+       else {
+          Node<T> newNode= new BinaryNode <> (data);
+           root.setLeftNode (newNode);
+          size++;
+           }
+       return "left";
+      }
+       else if (data.compareTo(root.getData()) > 0) {
+
+           if (root.getRightNode() != null) {
+               insertNew(data, root.getRightNode());
+           } else {
+               Node<T> newNode = new BinaryNode(data);
+               root.setRightNode(newNode);
+               size++;
+               System.out.println("INSERT" + data);
+           }
+           return "right";
+       }
+        return "no";
+
+    }
+
+
+
+
+    ///////////////////////
+    public boolean Contains(T data) {
+
+        if (data.compareTo (root.getData()) == 0)
+        {
+            return true;}
+        else if( data.compareTo (root.getData())>0)
+
+        {      System.out.println("CONTAINS8");
+            if(root.getRightNode () != null){
+                System.out.println("CONTAINS");
+         return    ContainsRecargin(data,root.getRightNode());}
+            else
+            return false;
+        }
+            else {
+            if(root.getLeftNode() != null) {
+                return ContainsRecargin(data, root.getLeftNode());
+            }
+            }
+    return false;}
+
+
+
+       ///////////////////////////////////
+       private boolean ContainsRecargin(T data, Node<T> root) {
+
+          if(cheack( data,  root)) {
+              return true;
+          }
+
+           if (root.getLeftNode() != null) {
+               if (cheack(data, root.getLeftNode()))
+                   return true;
+           }
+           if (data.compareTo (root.getData()) < 0)
+           {
+
+               if (root.getLeftNode () != null)
+               {
+                   if(cheack( data,  root.getLeftNode()))
+                   return true;
+                   ContainsRecargin(data, root.getLeftNode());
+               }
+               else {
+                   return false;
+               }
+           }
+           else if (data.compareTo(root.getData()) > 0) {
+               if (root.getRightNode() != null) {
+                   if(cheack( data,  root.getLeftNode()))
+                       return true;
+                   ContainsRecargin(data, root.getRightNode());
+               }
+               else {
+                   return false;
+               }
+
+           }
+           else {
+              return false; }
+
+return false;
+       }
+
+
+
+
+
+    public boolean cheack(T data, Node<T> root) {
+        if (data.compareTo (root.getData()) == 0)
+        { System.out.println(data+"*"+root.getData());
+            return true;}
+        else
+            return false;
+    }
+
+
+    public boolean isTreeEmpty() {
+            if(root == null)
+            {
+                return true;
+            }
+            return false ;
+           }
+
+}
